e802c107093943f238538bfaf3014e8e4ef681dc
aLEKS-e3 <chebukin404@gmail.com>
2024-05-17 17:42:29 +0300

    Create scraper class for detail link gathering

--- scraper.py
+++ scraper.py
@@ -27,3 +27,42 @@ class BaseScraper:
             "Version/4.0 Mobile Safari/533.1'"
         )
         return self.options
+
+class ResidentialThumbnailScraper(BaseScraper):
+    def _next_page(self):
+        next_button = self.driver.find_element(
+            By.CSS_SELECTOR, "li.next > a"
+        )
+        if next_button.is_displayed():
+            next_button.click()
+            time.sleep(0.25)
+
+    @staticmethod
+    def get_detail_url(thumbnail: WebElement) -> str:
+        link = WebDriverWait(thumbnail, 1).until(
+            EC.presence_of_element_located(
+                (By.CLASS_NAME, "a-more-detail")
+            )
+        )
+        return link.get_attribute("href")
+
+    def scrape_properties(self, url: str, amount: int) -> list:
+        self.driver.get(url)
+        detail_links = []
+
+        while True:
+            thumbnails = self.driver.find_elements(
+                By.CLASS_NAME, "property-thumbnail-item"
+            )
+            links = [
+                self.get_detail_url(thumbnail)
+                for thumbnail in thumbnails
+            ]
+            detail_links.extend(links)
+
+            if len(detail_links) >= amount:
+                break
+
+            self._next_page()
+
+        return detail_links[:amount]
