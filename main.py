7e485de39b45bbb39407251906bd8cd4104af67e
aLEKS-e3 <chebukin404@gmail.com>
2024-05-17 17:44:30 +0300

    Initialize main.py for effective usage

---
+++ main.py
@@ -0,0 +1,24 @@
+from file_manager import write_to_json
+from scraper import ResidentialThumbnailScraper, ResidentialAdvertScraper
+
+
+BASE_URL = "https://realtylink.org/en/properties~for-rent"
+ADVERTS_AMOUNT = 60
+
+
+def main(url: str, adverts_amount: int) -> list[dict]:
+    thumbnail_scraper = ResidentialThumbnailScraper()
+    detail_links = thumbnail_scraper.scrape_properties(url, adverts_amount)
+
+    property_scraper = ResidentialAdvertScraper()
+    details = [
+        property_scraper.scrape_advert(link)
+        for link in detail_links
+    ]
+
+    return details
+
+
+if __name__ == "__main__":
+    data = main(BASE_URL, ADVERTS_AMOUNT)
+    write_to_json("real-estate-example.json", data)
